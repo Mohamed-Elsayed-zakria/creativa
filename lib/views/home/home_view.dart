b3e922f578003f934a3d26d158a964390126c1bc
Mohamed Elsayed Zakria Saied <hagatonline24@gmail.com>
2024-02-01 14:45:48 +0200

    add buttom nav bar

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,10 +1,17 @@
+import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:flutter/material.dart';
-
 import 'widgets/home_view_body.dart';
+import '/shared/colors.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int pageNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +19,41 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Creativa'),
       ),
-      body: const HomeViewBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: AppColors.kPrimaryColor,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        currentIndex: pageNumber,
+        onTap: (value) {
+          setState(() {
+            pageNumber = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(IconlyBroken.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(IconlyBroken.category),
+            label: 'Services',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(IconlyBroken.notification),
+            label: 'Notice',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(IconlyBroken.buy),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(IconlyBroken.setting),
+            label: 'Settings',
+          ),
+        ],
+      ),
+      body: HomeViewBody(pageNumber: pageNumber),
     );
   }
 }
--- lib/views/home/widgets/home_view_body.dart
+++ lib/views/home/widgets/home_view_body.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
-  const HomeViewBody({super.key});
+  final int pageNumber;
+  const HomeViewBody({
+    super.key,
+    required this.pageNumber,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return IndexedStack(
+      index: pageNumber,
+      children: const [
+        Center(
+          child: Text(
+            'Home',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Services',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Notice',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Cart',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Settings',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ],
+    );
   }
 }
